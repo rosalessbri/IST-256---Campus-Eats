5337c5c424f9612f776cb949079e96de44f1ee6c
lilakakad <lilakakad11@gmail.com>
2026-04-18 20:20:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,3 +18,38 @@ db.connect(err =>{
 if(err) throw err;
 console.log('SQL connected');
 });
+
+const createTable = 
+`
+CREATE TABLE IF NOT EXISTS reviews (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(150) NOT NULL,
+year VARCHAR(20),
+dorm VARCHAR(100),
+phone VARCHAR(20),
+location VARCHAR(50) NOT NULL,
+resturant VARCHAR(100) NOT NULL,
+rating INT NOT NULL,
+comment TEXT);
+
+CREATE TABLE IF NOT EXISTS dining_locations(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(150) NOT NULL.
+description TEXT,
+hours VARCHAR(300),
+image_url VARCHAR(400));
+
+CREATE TABLE IF NOT EXISTS subscriptions(
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_email VARCHAR(150) NOT NULL,
+plan VARCHAR(50) NOT NULL,
+price DECIMAL(6,2) NOT NULL);
+`;
+
+db.query(createTables, err =>{
+    if(err) console.log('There was an error creating your table!', err.message);
+    else
+        console.log('Your tables were succesfully created');
+
+});
